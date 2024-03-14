d7c8c2a282c648e2d869d913838a8823e0a2ac68
realyun99 <realyun99@gmail.com>
2024-03-14 10:25:14 +0900

    spring test

---
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -0,0 +1,23 @@
+package com.example.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
+
+@RestController
+class Helloworld {
+	@GetMapping("/")
+	public String greet() {
+		return "Hello! This is Test Homepage";
+	}
+}
